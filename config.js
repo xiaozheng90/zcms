ce1411545a62136a79c24aa23a74ebfcc2a04959
zss <xiaozheng90@gmail.com>
2014-12-01 23:24:32 +0800

    能够利用sequelize读取model。

--- config.js
+++ config.js
@@ -4,7 +4,9 @@
 var config={
     cookieSecret:'zsslab',
     sessionDb:'mongodb://localhost/zsslab111?poolSize=20',
-    mysqlDb:'mysql://root@127.0.0.1:3306/zcms'
+    mysqlDb:'mysql://root:qinkin@127.0.0.1:3306/zcms',
+    appPath:'http://localhost:3000/',
+    baseDir:__dirname
 }
 module.exports = function(){
     return config;
--- services/dbManage.js
+++ services/dbManage.js
@@ -2,12 +2,13 @@
  * Created by lenovo on 2014/11/26.
  */
 var express=require("express");
+var path = require('path');
 var Sequelize=require("sequelize");
 var config=require("../config")();
 var sequelize = new Sequelize(config.mysqlDb);
 var dbOrm = function(tablename){
-
-    var model = sequelize.import("../models/"+tablename+".js");
+    var modelPath = path.join(config.baseDir,'/models/'+tablename+'.js');
+    var model = sequelize.import(modelPath);
     return model;
 };
 module.exports = dbOrm;
